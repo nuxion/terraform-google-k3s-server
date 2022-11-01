fd48fd5de774c51f577314b88ba407b7279eae2b
nuxion <nuxion@gmail.com>
2022-11-01 00:17:59 -0300

    fix restore script

--- files/restore.sh
+++ files/restore.sh
@@ -7,22 +7,7 @@ BACKUP_DIR=$(mktemp -d)
 
 mkdir -p ${SERVER_DIR}
 mkdir -p ${RANCHER_CONF}
-gstuil cp ${BUCKET}/${TARGZ} ${BACKUP_DIR}/${TARGZ}
-tar xvfz ${BACKUP_DIR}/${TARGZ} -C /
-mv ${SERVER_DIR}/db/backup.db $SERVER_DIR/db/state.db
-rm -Rf $BACKUP_DIR
-root@k3s-main:~# ^C
-root@k3s-main:~# cat restore.sh
-#!/bin/bash
-BUCKET=$1
-TARGZ=$2
-SERVER_DIR=/var/lib/rancher/k3s/server
-RANCHER_CONF=/etc/rancher
-BACKUP_DIR=$(mktemp -d)
-
-mkdir -p ${SERVER_DIR}
-mkdir -p ${RANCHER_CONF}
-gstuil cp ${BUCKET}/${TARGZ} ${BACKUP_DIR}/${TARGZ}
+gsutil cp ${BUCKET}/${TARGZ} ${BACKUP_DIR}/${TARGZ}
 tar xvfz ${BACKUP_DIR}/${TARGZ} -C /
 mv ${SERVER_DIR}/db/backup.db $SERVER_DIR/db/state.db
 rm -Rf $BACKUP_DIR
